1d8f4ea751df4741a9010d77dab81e5fb149f556
codewithme224 <codewithme224@gmail.com>
2022-12-08 00:46:01 -0800

    Hello, printf c program

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Main function
+ *
+ *
+ * Return: returns 0 everytime
+ */
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+
+}
