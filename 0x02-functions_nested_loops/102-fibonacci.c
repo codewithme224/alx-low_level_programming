fe541faf467b8250727f3ff68f5041b1ff375924
codewithme224 <codewithme224@gmail.com>
2022-12-13 12:11:41 -0800

    50 fibonacci numbers

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - entry point of the program
+ *
+ * Return: return 0
+ */
+int main(void)
+{
+	int count;
+	unsigned long f1 = 0, f2 = 1, f;
+
+	for (count = 0; count < 50; count++)
+	{
+		f = f1 + f2;
+		printf("lu", f);
+
+		f1 = f2;
+		f2 = f;
+
+		if (count == 49)
+			printf("\n");
+		else
+			printf(", ");
+	}
+
+	return (0);
+}
