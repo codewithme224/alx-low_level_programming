4efb766aa5323e11238b74f0ffeef998d1fdb6d7
codewithme224 <codewithme224@gmail.com>
2022-12-19 08:20:00 -0800

    Task 4 -  prints a string, in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string, in reverse
+ * @s: string parameter
+ *
+ * Return: return 0
+ */
+void print_rev(char *s)
+{
+	int characters;
+
+	for (characters = 0; s[characters] != '\0'; ++characters)
+		;
+
+	for (--characters; characters >= 0; --characters)
+		_putchar(s[index]);
+	_putchar('\n');
+}
