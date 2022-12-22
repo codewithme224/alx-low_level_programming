6ffede030786814fe5c4fd393c695c47b5a37b6b
codewithme224 <codewithme224@gmail.com>
2022-12-21 22:12:53 -0800

    Task 9 - prints an integer

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_number - function that prints an integer
+ *
+ * @n: integer parameter
+ *
+ * Return: return 0
+ */
+
+coid print_number(int n)
+{
+	unsigned int n1;
+
+	n1 = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n1 = -n;
+	}
+
+	if (n1 / 10 != 0)
+	{
+		print_number(n1 / 10);
+	}
+	_putchar((n1 % 10) + '0');
+}
