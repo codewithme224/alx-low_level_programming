4c741e70f088392b0957b9e45c59ca7fdde7b552
codewithme224 <codewithme224@gmail.com>
2023-01-03 05:41:17 -0800

    First Task

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -14,9 +14,9 @@
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int x;
+	unsigned int i;
 
-	for (x = 0; x < n; x++)
-		dest[x] = src[c];
+	for (i = 0; i < n; i++)
+		dest[i] = src[i];
 	return (dest);
 }
