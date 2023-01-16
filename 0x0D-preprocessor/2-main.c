fe8a29982454403a1e19e386021f61f80849fb1a
codewithme224 <codewithme224@gmail.com>
2023-01-15 23:20:23 -0800

    Task 2

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - print the name of the file program it was compiled from
+ * Return: return 0
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
