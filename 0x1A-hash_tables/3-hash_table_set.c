f2c18a30ba82979a17cc54c433da16adf5bc4cc5
codewithme224 <codewithme224@gmail.com>
2023-04-07 04:21:08 -0700

    adds an element to the hash table.

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -33,25 +33,14 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		temp = temp->next;
 	}
 
-	new_node = malloc(sizeof(hash_node_t));
+	/*Creating a new node with key/value pairs*/
+	new_node = (hash_node_t *) malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
+	{
 		return (0);
-
+	}
 	new_node->key = strdup(key);
-	if (new_node->key == NULL)
-	{
-		free(new_node);
-		return (0);
-	}
-
 	new_node->value = strdup(value);
-	if (new_node->value == NULL)
-	{
-		free(new_node->key);
-		free(new_node);
-		return (0);
-	}
-
 	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
 
