552de984d98105d8817fcb3f5639c8a0d5348419
codewithme224 <codewithme224@gmail.com>
2023-07-11 00:11:22 -0700

    Linear Search

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -2,13 +2,13 @@
 
 
 /**
- * linear_search - searches for a value in an array of 
+ * linear_search - searches for a value in an array of
  *                 integers using the Linear search algorithm
  * @array: pointer to the first element of the array to search in
  * @size: number of elements in array
  * @value: value to search for
  *
- * Return: first index where value is located. If value is not 
+ * Return: first index where value is located. If value is not
  *         present in array or if array is NULL, return -1
  */
 
